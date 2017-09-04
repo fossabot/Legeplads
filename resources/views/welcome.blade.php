7ce436f9ebd868bdd578b7d536d6e08f5dfd2c62
Lasse Nielsen <lassexl20@gmail.com>
2017-09-04 15:25:07 +0200

    Lol

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Lasse's Legeplads
                 </div>
 
                 <div class="links">
