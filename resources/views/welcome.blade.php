14fda6707c84ef620762e385b596fc083c2b4562
Lasse Nielsen <lassexl20@gmail.com>
2017-09-04 15:28:40 +0200

    Lol

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,6 +81,9 @@
                 <div class="title m-b-md">
                     Lasse's Legeplads
                 </div>
+                <div class="subtitle m-b-md">
+                    Her vil komme noget en dag!
+                </div>
             </div>
         </div>
     </body>
