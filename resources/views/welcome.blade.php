d4de19a0638864818fc5b41276e1b1e251024b97
Lasse Nielsen <lassexl20@gmail.com>
2017-09-20 09:22:36 +0200

    Contact

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,4 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Lasse's Legeplads</title>
-        <!-- Styles -->
-        <link href="css/app.css" rel="stylesheet" type="text/css">
-    </head>
+{{ include}}
     <body>
         <div class="flex-center full-height">
             <div class="content">
@@ -18,6 +8,43 @@
                 <div class="subtitle m-b-md">
                     Her vil komme noget en dag!
                 </div>
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+
+                {!! Form::open(array('route' => 'contact_store', 'class' => 'form')) !!}
+
+                <div class="form-group">
+                    {!! Form::label('Your Name') !!}
+                    {!! Form::text('name', null,
+                        array('required',
+                              'class'=>'form-control',
+                              'placeholder'=>'Your name')) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('Your E-mail Address') !!}
+                    {!! Form::text('email', null,
+                        array('required',
+                              'class'=>'form-control',
+                              'placeholder'=>'Your e-mail address')) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('Your Message') !!}
+                    {!! Form::textarea('message', null,
+                        array('required',
+                              'class'=>'form-control',
+                              'placeholder'=>'Your message')) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::submit('Contact Us!',
+                      array('class'=>'btn btn-primary')) !!}
+                </div>
+                {!! Form::close() !!}
             </div>
         </div>
     </body>
