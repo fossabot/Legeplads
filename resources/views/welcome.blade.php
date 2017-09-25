32aa9b3bf3aa4f34fe688d6d8f7b02504cd857fe
Lasse Nielsen <lassexl20@gmail.com>
2017-09-25 19:39:41 +0200

    Doing new stuff

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,11 +12,8 @@
     <body>
         <div class="flex-center full-height">
             <div class="content">
-                <div class="title m-b-md">
-                    Lasse's Legeplads
-                </div>
-                <div class="subtitle m-b-md">
-                    Her vil komme noget en dag!
+                <div class="title m-b-md animated pulse infinite">
+                    STREAM STARTING SOON-ISH
                 </div>
             </div>
         </div>
