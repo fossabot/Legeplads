a2a6d2c083ec524b3905c4167895c103208338e6
Lasse Nielsen <lassexl20@gmail.com>
2017-10-08 14:57:56 +0200

    Doing new stuff

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,17 +9,21 @@
     <!-- Styles -->
     <link href="css/app.css" rel="stylesheet" type="text/css">
 </head>
-    <body>
-    <header>
+<body>
+<header>
+    {{ $yes = true }}
+    @if($yes === true)
         @include('layout.components.menu')
-    </header>
-        <div class="flex-center full-height">
-            <div class="content">
-                <div class="title m-b-md animated pulse infinite">
-                    STREAM STARTING SOON-ISH
-                </div>
-            </div>
+    @else
+    @endif
+</header>
+<div class="flex-center full-height">
+    <div class="content">
+        <div class="title m-b-md animated pulse infinite">
+            STREAM STARTING SOON-ISH
         </div>
-    <script href="js/app.js" type="text/javascript"></script>
-    </body>
+    </div>
+</div>
+<script href="js/app.js" type="text/javascript"></script>
+</body>
 </html>
